c9051cd1b5405bbf78c5031b83d8cfe65173ff30
Johannes <gruber@europa-uni.de>
2022-04-26 17:59:35 +0200

    first commit

---
+++ bench.r
@@ -0,0 +1,53 @@
+### setup
+library(quanteda)
+library(stm)
+library(furrr)
+
+corp <- quanteda.corpora::download("data_corpus_guardian")
+time <- c(start = Sys.time())
+
+### Matrix
+N <- 10e3
+M <- 2000
+X <- matrix(rnorm(N * M), N)
+time <- c(time, matrix = Sys.time())
+
+### preprocessing
+test_dfm <- corp %>% 
+  tokens() %>% 
+  tokens_remove(stopwords("en")) %>% 
+  dfm() %>% 
+  dfm_trim(min_termfreq = 10)
+time <- c(time, nlp_prep = Sys.time())
+
+### STM
+test_mod <- stm(test_dfm, K = 100)
+time <- c(time, stm_single = Sys.time())
+
+### STM-multi
+plan(multicore, workers = 4)
+many_models <- data_frame(K = seq(40, 60, 5)) %>%
+  mutate(topic_model = future_map(K, ~stm(test_dfm, K = .,
+                                          verbose = FALSE)))
+time <- c(time, stm_multi = Sys.time())
+
+### report
+library(tidyverse)
+tibble(
+  stage = names(time),
+  time_stamp = time
+) %>% 
+  mutate(time = time_stamp - lag(time_stamp),
+         id = 1) %>% 
+  drop_na(time) %>% 
+  select(stage, time) %>% 
+  pivot_wider(names_from = "stage", values_from = "time") %>% 
+  mutate(session = list(sessionInfo())) %>% 
+  {
+    if (file.exists("bench_results.rds")) {
+      bind_rows(readRDS("bench_results.rds"), .)
+    } else {
+      .
+    }
+  } %>% 
+  saveRDS("bench_results.rds")
---
+++ readme.md
@@ -0,0 +1,5 @@
+# a qucik R script to benchmark processor performance
+
+Relying mostly on NLP tasks performed in quanteda and stm.
+
+Run with `Rscript bench.r`.
